ad31c7dac3941af240530408850616b6497805fc
Ashwin <ashwingairola999@gmail.com>
2020-07-23 11:41:01 +0530

    57. Lists and State

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -13,18 +13,6 @@ class App extends Component {
 		showPersons: false
 	};
 
-	switchNameHandler = (newName) => {
-		// console.log('Was clicked!');
-		// DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-		this.setState({
-			persons: [
-				{ name: newName, age: 28 },
-				{ name: 'Manu', age: 29 },
-				{ name: 'Stephanie', age: 27 }
-			]
-		});
-	};
-
 	nameChangedHandler = (event) => {
 		this.setState({
 			persons: [
@@ -33,6 +21,12 @@ class App extends Component {
 				{ name: 'Stephanie', age: 27 }
 			]
 		});
+	};
+
+	deletePersonHandler = (personIndex) => {
+		const persons = this.state.persons;
+		persons.splice(personIndex, 1);
+		this.setState({ persons });
 	};
 
 	togglePersonsHandler = () => {
@@ -55,7 +49,12 @@ class App extends Component {
 			persons = (
 				<div>
 					{this.state.persons.map((person, i) => (
-						<Person name={person.name} age={person.age} key={i} />
+						<Person
+							name={person.name}
+							age={person.age}
+							key={i}
+							click={() => this.deletePersonHandler(i)}
+						/>
 					))}
 					{/* <Person
 						name={this.state.persons[0].name}
