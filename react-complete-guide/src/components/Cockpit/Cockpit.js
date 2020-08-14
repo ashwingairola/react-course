0876a44bbdc5688949bec07fce1c1d35caf57b29
Ashwin <ashwingairola999@gmail.com>
2020-08-14 21:34:28 +0530

    94. Controlling the useEffect() Behavior

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -3,9 +3,29 @@ import React, { useEffect } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+	/*
+        Pass a list of dependencies as the 2nd argument. The component
+        will update only if those dependencies change.
+    */
+	// useEffect(() => {
+	// 	console.log('[Cockpit.js] useEffect');
+
+	// 	setTimeout(() => {
+	// 		console.log('Saved data to cloud');
+	// 	}, 1000);
+	// }, [props.persons]);
+
+	/*
+        Pass an empty list to make useEffect invoke only once - 
+        when the component is mounted.
+    */
 	useEffect(() => {
 		console.log('[Cockpit.js] useEffect');
-	});
+
+		setTimeout(() => {
+			console.log('Saved data to cloud');
+		}, 1000);
+	}, []);
 
 	let assignedClasses = [];
 	let btnClass = [];
