81aa72ee7e75448bd403cbccadb3af813b4467ea
Ashwin <ashwingairola999@gmail.com>
2020-09-17 23:54:49 +0530

    260. Dispatching Actions from Within the Reducer

--- redux-basics/src/store/reducer.js
+++ redux-basics/src/store/reducer.js
@@ -3,6 +3,11 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case 'INCREMENT':
+			return { ...state, counter: state.counter + 1 };
+	}
+
 	return state;
 };
 
