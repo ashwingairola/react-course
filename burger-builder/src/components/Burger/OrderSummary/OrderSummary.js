face483d0ed3ec3d86c35de38ee8c54c28c93bf3
Ashwin <ashwingairola999@gmail.com>
2020-09-19 12:57:49 +0530

    137. Creating the Order Summary Modal

---
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import classes from './OrderSummary.module.css';
+import Aux from '../../../hoc/Aux';
+
+const orderSummary = (props) => {
+	const ingredientSummary = Object.keys(props.ingredients).map((key) => {
+		return (
+			<li key={key}>
+				<span className={classes.Ingredient}>{key}</span>:{' '}
+				{props.ingredients[key]}
+			</li>
+		);
+	});
+
+	return (
+		<Aux>
+			<h3>Your Order</h3>
+			<p>A delicious burger with the following ingredients:</p>
+			<ul>{ingredientSummary}</ul>
+			<p>Continue to Checkout?</p>
+		</Aux>
+	);
+};
+
+export default orderSummary;
